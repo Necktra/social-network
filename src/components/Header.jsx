3bc36c7dad29584541a96571a9d042c25ce442d0
Ira <hallo.ween@mail.ru>
2021-08-06 14:32:07 +0300

    nav header profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (<header className={classes.header}>
+        <img src='https://i.pinimg.com/originals/d9/3f/34/d93f341355683d63e28a63ec2bae31f6.png' width='50' height='40'></img>
+    </header>
+    )
+}
+
+export default Header;
