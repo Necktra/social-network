d6c84c267efee3f264cbd1dc2125c256cd1cfbfd
Irina <hallo.ween@mail.ru>
2021-08-08 22:06:21 +0300

    Dialogs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import News from './components/News/News';
 import {BrowserRouter, Route} from "react-router-dom";
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 function App() {
   return (
@@ -14,7 +17,10 @@ function App() {
       <Navbar />
       <div class="app-wrapper-content">
         <Route path="/dialogs" component={Dialogs} />
-        <Route path="/Profile" component={Profile} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/news" component={News} />
+        <Route path="/music" component={Music} />
+        <Route path="/settings" component={Settings} />
       </div>
     </div>
     </BrowserRouter>
