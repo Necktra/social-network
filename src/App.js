21bfe56aa83f773f4e239620b1fecc5771c12ea6
Irina <hallo.ween@mail.ru>
2021-08-08 20:43:21 +0300

    Profile + routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
+      <div class="app-wrapper-content">
+        <Route path="/dialogs" component={Dialogs} />
+        <Route path="/Profile" component={Profile} />
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import MyPosts from './MyPosts/MyPosts';
 import classes from './Profile.module.css';
 
 const Profile = () => {
-    return (<div className={classes.content}>
+    return (<div>
         <img src='https://i.pinimg.com/originals/de/30/de/de30dec00431f4b36c09cdd7dafd01e5.jpg'></img>
         <div>Ava + description</div>
         <MyPosts />
