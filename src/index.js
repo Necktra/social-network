6327c3ac389f8105c1a94b465808cd9832447424
Ira <hallo.ween@mail.ru>
2021-08-18 13:02:01 +0300

    profile-reducer state copy update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 // import {addPost, sendMessage, updateNewMessageText, updateNewPostText} from './redux/state';
 //  {/* <App state={state} dispatch={store.dispatch.bind(store)} sendMessage={store.sendMessage.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)}/> */}
-let rerenderEntireTree = (state) => {
+//let rerenderEntireTree = (state) => {
 ReactDOM.render(
   <React.StrictMode>
     <Provider store = {store}>
@@ -20,10 +20,11 @@ ReactDOM.render(
 document.getElementById('root')
 );
  
-};
-rerenderEntireTree(store.getState());
+// };
+// rerenderEntireTree(store.getState());
 
-store.subscribe(()=>{rerenderEntireTree(store.getState())});
+// store.subscribe(()=>{rerenderEntireTree(store.getState())});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
