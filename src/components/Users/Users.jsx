eb3fee6cd1a6d9728bf42b94889bb608346ce668
Ira <hallo.ween@mail.ru>
2021-08-19 16:43:59 +0300

    Users class componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,12 +5,10 @@ import React from "react";
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            })
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
     getUsers = () => {
