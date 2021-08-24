550829bf1cfc267355ff4c1c5b31e4de89ec2b2d
Ira <hallo.ween@mail.ru>
2021-08-24 15:03:06 +0300

    ProfileInfo profile information interface update

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -6,8 +6,13 @@ const ProfileInfo = (props) => {
             <img className={classes.imgWall} src='https://i.pinimg.com/originals/de/30/de/de30dec00431f4b36c09cdd7dafd01e5.jpg'></img>
         </div>
         <div className={classes.descriptionBlock}>
-            <img src={props.profile.photos.large}/>
-            Ava + description</div>
+            <img src={props.profile.photos.large} />
+            {props.profile.aboutMe}</div>
+        <div>
+            FB: {props.profile.contacts.facebook}<br />
+            VK: {props.profile.contacts.vk}<br />
+            Instagram: {props.profile.contacts.instagram}<br />
+        </div>
     </div>
     )
 }
