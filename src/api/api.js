96d1dc87a6a2c7801426309c8f1c94b980e4eac0
Ira <hallo.ween@mail.ru>
2021-08-30 13:01:16 +0300

    DAL + axios creator for getUsers

---
+++ src/api/api.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "e7da8bc7-fe26-4fcc-96fb-8aca7e2700ec"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+            return response.data
+        });
+    }
+}
+
+// export const getUsers = (currentPage = 1, pageSize = 10) => {
+//     return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+//         return response.data
+//     });
+// }
