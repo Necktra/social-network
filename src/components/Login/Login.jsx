08f48b2179155f52927558ee74f5afbe51a01fb1
Irina <hallo.ween@mail.ru>
2021-09-29 21:08:33 +0300

    form for Login update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { Field, reduxForm } from "redux-form";
 
 const LoginForm = (props) => {
-    return (<form>
+    return (<form onSubmit={props.handleSubmit}>
             <div> <Field placeholder={"Login"} name={"login"} component={"input"}/> </div>
             <div> <Field placeholder={"Password"} name={"password"} component={"input"}/> </div>
             <div> <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me</div>
@@ -12,9 +12,14 @@ const LoginForm = (props) => {
 const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
 const Login = (props) => {
+    
+    const onSubmit = (formData) => {
+        console.log(formData);
+    };
+
     return (<div>
         <h1>LOGIN</h1>
-        <LoginReduxForm />
+        <LoginReduxForm onSubmit={onSubmit}/>
     </div>)
 }
 
