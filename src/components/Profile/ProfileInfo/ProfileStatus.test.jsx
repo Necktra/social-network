bd306c2d45810d21adf41ae08ebc2e35c5959879
Irina <hallo.ween@mail.ru>
2021-10-04 19:17:23 +0300

    unit-test for ProfileStatus update

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -28,5 +28,23 @@ describe("ProfileStatus component", () => {
         const root = component.root;
         let span = root.findByType("span");
         expect(span.children[0]).toBe("it-kamasutra.com");
-    })
+    });
+
+    test ("Input should be displayed in edit mode instead of span", () => {
+        const component = create(<ProfileStatus status="it-kamasutra.com"/>);
+        const root = component.root;
+        let span = root.findByType("span");
+        span.props.onDoubleClick();
+        let input = root.findByType("input");
+        expect(input.props.value).toBe("it-kamasutra.com");
+    });
+
+    test ("Callbach should be called", () => {
+        const mockCallback = jest.fn();
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatus={mockCallback}/>);
+        const instance = component.getInstance();
+        instance.deactivateEditMode();     
+        expect(mockCallback.mock.calls.length).toBe(1);
+    });
+
 });
