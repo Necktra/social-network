9ec4e7fcd49a7a5a416d48fc6baeca5a2b7cd5b9
Ira <hallo.ween@mail.ru>
2021-10-05 10:46:26 +0300

    Paginator update

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,10 @@
-import styles from "./Users.module.css";
-import userPhoto from "../../assets/images/user.png";
 import React from "react";
-import { NavLink } from "react-router-dom";
 import Paginator from "../common/Paginator/Paginator";
 import User from "./User";
 
 let Users = ({ currentPage, onPageChanged, totalUsersCount, pageSize, users, ...props }) => {
     return <div>
-        <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalUsersCount={totalUsersCount} pageSize={pageSize} />
+        <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalItemsCount={totalUsersCount} pageSize={pageSize} />
         <div>
             {users.map(u => <User user={u} followingInProgress={props.followingInProgress}
                 unfollow={props.unfollow} follow={props.follow}
